8dac538187722110529e203682c0f9d5b146c3b4
ArtemKosyrev <artemkosyrev93@gmail.com>
2025-09-22 15:27:48 +0500

    feat: (sign-in) add css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import logo from '$lib/assets/logo.png';
 	import {Role} from "$lib/enums/role.enum";
 
 	let { children, data } = $props();
@@ -14,9 +15,11 @@
 	<title>Home</title>
 </svelte:head>
 
-<div>
+<header>
 	<nav>
-		<a href="/">Logo</a>
+		<div class="logo-container">
+			<a href="/"><img src={logo}></a>
+		</div>
 		<ul>
 			{#if data?.user !== null}
 				{#if !isAdministrator}
@@ -24,16 +27,123 @@
 					<li><a href="/#">Calendar</a></li>
 					<li><a href="/notifications">Notifications <span>{countNotifications}</span></a></li>
 				{/if}
-				<form action="/logout" method="POST" style="display:inline" >
-					<button type="submit">Выйти</button>
-				</form>
+
 			{:else}
 				<li><a href="/sign-up">Start Now</a></li>
 				<li><a href="/sign-in">Sign In</a></li>
 			{/if}
-
 		</ul>
+		{#if data?.user !== null}
+			<form action="/logout" method="POST" class="form-container">
+				<button type="submit">Выйти</button>
+			</form>
+		{/if}
+
 	</nav>
+</header>
+
+<div class="main-container">
+	{@render children?.()}
 </div>
 
-{@render children?.()}
+<style>
+	header {
+		height: 100%;
+		width: 250px;
+		position: fixed;
+		z-index: 1;
+		top: 0;
+		left: 0;
+		overflow-x: hidden;
+		background-color: #2d2d2d;
+		padding: 30px 10px;
+		box-sizing: border-box;
+		box-shadow: 15px 0 10px 0 rgba(0, 0, 0, 0.1);
+	}
+
+	.main-container {
+		margin-left: 250px;
+		padding: 30px;
+		background-color: rgb(22, 22, 22);
+		height: 100vh;
+		color: white;
+		box-sizing: border-box;
+	}
+
+	nav {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		row-gap: 20px;
+	}
+
+	.logo-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 200px;
+	}
+	img {
+		width: auto;
+		height: 100px;
+		transition: all 0.2s;
+	}
+	img:hover {
+		width: auto;
+		height: 110px;
+	}
+
+	nav ul {
+		padding: 0;
+		margin: 0;
+		display: flex;
+		flex-direction: column;
+		row-gap: 10px;
+		font-size: 1.2em;
+		font-weight: bold;
+	}
+
+	li {
+		padding: .5em;
+		border-radius: 5px;
+		background-color: #F0BA3A;
+		border: 0.2em solid #F0BA3A ;
+		transition: all 0.3s;
+	}
+
+	li:hover {
+		border: 0.2em solid rgba(255, 255, 255, 0.5);
+	}
+
+	li a {
+		display: block;
+		width: 100%;
+		height: 100%;
+	}
+
+	form {
+		display: flex;
+		justify-content: center;
+		height: 100%;
+	}
+
+	form button {
+		margin-top: auto;
+		padding: .5em 1.2em;
+		border-radius: 30px;
+		border: 1px solid #ce4141;
+		background-color: #ce4141;
+		font-family: "VAG World", sans-serif;
+		font-weight: bold;
+		font-size: 1.2em;
+		color: #ffffff;
+	}
+
+	form button:hover {
+		border-color: #ce4141;
+		background-color: rgba(255, 255, 255, 0);
+		cursor: pointer;
+	}
+
+</style>
--- src/routes/sign-up/+page.svelte
+++ src/routes/sign-up/+page.svelte
@@ -23,19 +23,19 @@
     <title>Sign Up</title>
 </svelte:head>
 
-<div>
+<div class="register-container">
     <h2>Регистрация</h2>
 
     <form action="?/register" method="POST" use:enhance>
-        <div>
+        <div class="user-box">
             <label for="email">Почта</label>
-            <input name="email" bind:value={email} type="text">
+            <input name="email" bind:value={email} type="email">
         </div>
-        <div>
+        <div class="user-box">
             <label for="fullName">Полное имя</label>
             <input name="fullName" bind:value={fullName} type="text">
         </div>
-        <div>
+        <div class="user-box">
             <label for="department">Отдел</label>
             <select name="department" id="department" bind:value={selectedDepartment}>
                 {#each data.departments as dep}
@@ -43,7 +43,7 @@
                 {/each}
             </select>
         </div>
-        <div>
+        <div class="user-box">
             <label for="role">Роль</label>
             <select name="role" id="role" bind:value={selectedRole}>
                 {#each roles as rol}
@@ -51,12 +51,85 @@
                 {/each}
             </select>
         </div>
-        <div>
+        <div class="user-box">
             <label for="password">Пароль</label>
-            <input name="password" bind:value={password} type="text">
+            <input name="password" bind:value={password} type="password">
         </div>
 
         <button type="submit">Зарегистрироваться</button>
     </form>
-
 </div>
+
+<style>
+    .register-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+
+    h2 {
+        font-size: 3em;
+        font-weight: 800;
+        color: #8DC63F;
+    }
+
+    form {
+        max-width: 400px;
+        width: 100%;
+    }
+
+    .user-box {
+        margin-bottom: 1em;
+        width: 100%;
+    }
+    .user-box label {
+        display: block;
+        margin-bottom: .5em;
+    }
+
+    .user-box input[type="text"],
+    .user-box input[type="email"],
+    .user-box input[type="password"],
+    .user-box select {
+        color: white;
+        width: 100%;
+        box-sizing: border-box;
+        padding: 1em;
+        border: none;
+        border-bottom: 3px solid #ccc;
+        background-color: rgba(255, 255, 255, 0);
+        font-size: 1em;
+        transition: all 0.3s;
+    }
+
+    .user-box input[type="text"]:focus,
+    .user-box input[type="email"]:focus,
+    .user-box input[type="password"]:focus,
+    .user-box select:focus {
+        border: 3px solid #f8e940;
+        outline: none;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+    }
+
+    form button[type="submit"] {
+        margin-top: 2.5em;
+        width: 100%;
+        padding: 1em;
+        font-size: 1em;
+        font-weight: 800;
+        background-color: #F0BA3A;
+        border-radius: 15px;
+        border: 1px solid rgb(255, 255, 255);
+        color: white;
+    }
+
+    form button[type="submit"]:hover {
+        cursor: pointer;
+        box-shadow: 0 0 5px 5px rgba(255, 255, 255, 0.2);
+    }
+
+
+</style>
